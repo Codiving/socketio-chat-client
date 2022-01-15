0ca92999fc1c5c753bdd33df1f2e62ee115319db
Codiving <codivingmoon@gmail.com>
2022-01-15 17:22:48 +0900

    6. 채팅하기

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,5 +1,14 @@
 import { useEffect, useState } from "react";
 
+const ChatWrapCss = {
+  marginBottom: 10,
+  width: 200,
+  height: 300,
+  overflowY: "scroll",
+  border: "1px solid",
+  padding: 3
+};
+
 const Chat = props => {
   const { roomName, socket } = props;
   const [chat, setChat] = useState("");
@@ -9,20 +18,29 @@ const Chat = props => {
     socket.on("join-msg", msg => {
       alert(msg);
     });
+
+    socket.on("message", msg => {
+      setList(prev => prev.concat({ me: false, text: msg }));
+    });
   }, [socket]);
 
   return (
     <div>
       <span>{roomName} 채팅방입니다.</span>
-      <div>
-        {list.map((text, index) => (
-          <p key={index}>{text}</p>
+      <div style={ChatWrapCss}>
+        {list.map((item, index) => (
+          <p key={index} style={item.me ? { textAlign: "right" } : {}}>
+            {item.text}
+          </p>
         ))}
       </div>
       <form
         onSubmit={e => {
           e.preventDefault();
-          setChat("");
+          socket.emit("message", chat, roomName, () => {
+            setList(prev => prev.concat({ me: true, text: chat }));
+            setChat("");
+          });
         }}
       >
         <input value={chat} onChange={e => setChat(e.target.value)} />
